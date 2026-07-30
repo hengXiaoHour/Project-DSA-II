ed47c32312f61c7fe143e353005d7a8c9c2eec67
hengXiaoHour <hengXiaoHour@users.noreply.github.com>
2026-07-30 08:02:07 +0700

    test sync with collaborator

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from frontend.app import app
 
 if __name__ == "__main__":
     print("RUPP Campus Navigation — Open http://localhost:5000 in your browser")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000
