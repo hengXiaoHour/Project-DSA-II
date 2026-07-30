c7ba26f878c1bd98e7f6263fccda4f37cb1e076f
hengXiaoHour <hengXiaoHour@users.noreply.github.com>
2026-07-30 09:28:58 +0700

    update document, update joints and walk path

--- script_dev/push.py
+++ script_dev/push.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import subprocess, sys
 
-msg = " ".join(sys.argv[1:]) or "update"
+msg = " ".join(sys.argv[1:]) or "update document, update joints and walk path"
 subprocess.run(f'git add -A && git commit -m "{msg}" && git push origin V2', shell=True, check=True)
